93f5d09919178db71b1184e8ce47975780971a58
asad <asadzahid1479@gmail.com>
2023-01-13 20:55:07 +0500

    setting up the base

---
+++ src/main/java/com/mylearning/education_class_database/EducationClassDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.education_class_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationClassDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationClassDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylearning/education_class_database/EducationClassDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.education_class_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationClassDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
